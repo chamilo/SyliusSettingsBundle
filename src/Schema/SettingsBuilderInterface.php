0c530b1c681698f8ba6e852b5dc335b3cf7ed2f6
Julio <gugli100@gmail.com>
2019-12-19 09:25:23 +0100

    Organise directory structure of the project

---
+++ src/Schema/SettingsBuilderInterface.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\SettingsBundle\Schema;
+
+use Symfony\Component\Form\DataTransformerInterface;
+
+/**
+ * @author Paweł Jędrzejewski <pawel@sylius.org>
+ */
+interface SettingsBuilderInterface
+{
+    /**
+     * @return DataTransformerInterface[]
+     */
+    public function getTransformers();
+
+    /**
+     * @param string                   $parameterName
+     * @param DataTransformerInterface $transformer
+     */
+    public function setTransformer($parameterName, DataTransformerInterface $transformer);
+}
